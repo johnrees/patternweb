5e0acd25611b472973b41d1a095f559bcd49211e
John Rees <john@bitsushi.com>
2017-06-25 00:54:12 +0100

    simplified graph, obj literal needs changing to prototype

--- src/__tests__/graph.js
+++ src/__tests__/graph.js
@@ -1,30 +1,30 @@
 const Graph = require("../graph")
 
-it("can be created without blowing up", () => expect(Graph()).toBeTruthy())
+it("can be created without blowing up", () => expect(new Graph()).toBeTruthy())
 
 describe("adding nodes", () => {
   it("can add a node", () => {
-    const g = Graph()
+    const g = new Graph()
     g.add("newnode")
     expect( Object.keys(g.nodes()) ).toEqual(["newnode"])
   })
 
   it("can only add a node with a unique ID", () => {
-    const g = Graph()
+    const g = new Graph()
     g.add("newnode")
     expect( () => g.add("newnode") ).toThrow("node already exists with that ID")
   })
 })
 
 it("can remove a node", () => {
-  const g = Graph()
+  const g = new Graph()
   g.add("newnode")
   g.remove("newnode")
   expect(Object.keys(g.nodes())).toHaveLength(0)
 })
 
 it("can find a node", () => {
-  const g = Graph()
+  const g = new Graph()
   g.add("newnode")
   expect(g.find("newnode")).toBeTruthy()
   expect(g.find("newnode2")).toBeUndefined()
@@ -39,7 +39,7 @@ it("can connect nodes", () => {
 })
 
 it("can disconnect nodes", () => {
-  const g = Graph()
+  const g = new Graph()
   g.add("nodeA")
   g.add("nodeB")
   g.connect("nodeA","nodeB")
--- src/graph.js
+++ src/graph.js
@@ -1,27 +1,34 @@
 const { Map } = require('immutable')
+const util = require('util')
+const EventEmitter = require('events')
 
-const Graph = () => {
+function Graph() {
 
   let _nodes = Map()
   let _edges = Map()
+  const events = new EventEmitter()
 
   const add = (nodeID/*, component, inputs*/) => {
     if (_nodes.has(nodeID)) throw "node already exists with that ID"
     _nodes = _nodes.set(nodeID, 1)
+    events.emit('add', { nodeID })
   }
 
   const find = nodeID => _nodes.get(nodeID)
 
   const remove = nodeID => {
     _nodes = _nodes.delete(nodeID)
+    events.emit('remove', { nodeID })
   }
 
   const connect = (sourceNodeID, targetNodeID) => {
     _edges = _edges.set(`${sourceNodeID}-${targetNodeID}`, 1)
+    events.emit('connect', { sourceNodeID, targetNodeID })
   }
 
   const disconnect = (sourceNodeID, targetNodeID) => {
     _edges = _edges.delete(`${sourceNodeID}-${targetNodeID}`)
+    events.emit('disconnect', { sourceNodeID, targetNodeID })
   }
 
   return {
@@ -30,9 +37,12 @@ const Graph = () => {
     remove,
     connect,
     disconnect,
+    events,
     nodes: () => _nodes.toJS(),
     edges: () => _edges.toJS()
   }
 }
+
+// util.inherits(Graph, EventEmitter)
 
 module.exports = Graph
