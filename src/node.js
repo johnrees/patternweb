7b3e64f3c2edc35080120df06853b22625eb8c7e
John Rees <john@bitsushi.com>
2017-06-29 10:15:57 +0100

    temporarily export newinputs

--- src/node.js
+++ src/node.js
@@ -6,12 +6,12 @@ function Node(nodeID, component, inputs) {
 
 Node.prototype.run = function(storeAccessor, done) {
 
-  const newInputs = Object.keys(this.inputs).reduce((chain, key) => {
+  this.newInputs = Object.keys(this.inputs).reduce((chain, key) => {
     chain[key] = storeAccessor(this.inputs[key])
     return chain
   }, {})
 
-  return this.component.fn(newInputs, done)
+  return this.component.fn(this.newInputs, done)
 }
 
 module.exports = Node
